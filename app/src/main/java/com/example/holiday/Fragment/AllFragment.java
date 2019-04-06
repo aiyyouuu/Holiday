b6ff3fa02fe6a529a0edd23e17dc3c841b379b2e
aiyyouuu <ilhamisuciayudyah@gamil.com>
2019-04-06 16:24:28 +0700

    message

--- app/src/main/java/com/example/holiday/Fragment/AllFragment.java
+++ app/src/main/java/com/example/holiday/Fragment/AllFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,6 +41,7 @@ public class AllFragment extends Fragment implements MainView {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rv1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
         adapter = new AllAdapter(getContext(), allModels);
         recyclerView.setAdapter(adapter);
         AllPresenter data = new AllPresenter(this,getContext());
